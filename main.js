1f88a6920438d285e644e19c97f1a782440225e3
rachitkhope <106763102+rachitkhope@users.noreply.github.com>
2023-05-04 12:51:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function addUser(){
+    var user_name=document.getElementById("user_name").value;
+    localStorage.setItem("user_name",user_name); 
+    //add chat_room.html
+    window.location="Chat_Room.html";
+    
+    }
